505fe52b9c3f905c5a7f1ecfdc077b9e13f43af8
mehmood <mehmoodalam60632@gmail.com>
2022-05-28 00:38:33 +0500

    ...

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ form.addEventListener('submit', (e) => {
 
     var task_content_el = document.createElement("div");
     task_content_el.classList.add("content");
+    // task_content_el.innerText = task;
     
 
     task_el.appendChild(task_content_el);
@@ -24,32 +25,42 @@ form.addEventListener('submit', (e) => {
     task_input_el.type = "text";
     task_input_el.value = task;
     task_input_el.setAttribute("readonly", "readonly");
-
+    
     task_content_el.appendChild(task_input_el);
-
+    
     var task_actions_el = document.createElement("div");
     task_actions_el.classList.add("actions");
-
+    
     var task_edit_el = document.createElement("button");
     task_edit_el.classList.add("edit");
     task_edit_el.innerHTML = "Edit";
-
+    
     var task_delete_el = document.createElement("button");
     task_delete_el.classList.add("delete");
     task_delete_el.innerHTML = "Delete";
-
+    
     task_actions_el.appendChild(task_edit_el);
     task_actions_el.appendChild(task_delete_el);
-
+    
     task_el.appendChild(task_actions_el);
-
+    
     list_el.appendChild(task_el);
 
     input.value = "";
 
-    task_delete_el.addEventListener('click', () => {
+    task_edit_el.addEventListener('click', () => {
+       if(task_edit_el.innerText.toLowerCase() == "edit") {
         task_input_el.removeAttribute("readonly");
         task_input_el.focus();
-    })
-})
-})
+        task_edit_el.innerText = "save";
+       } else {
+           task_input_el.setAttribute("readonly", "readonly");
+           task_edit_el.innerText = "Edit";
+       }
+    });
+
+    task_delete_el.addEventListener('click', () => {
+        list_el.removeChild(task_el);
+    });
+});
+});

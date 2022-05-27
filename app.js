665ea9f7f5ee4bea91225f6ca028274b66379b14
mehmood <mehmoodalam60632@gmail.com>
2022-05-27 21:02:25 +0500

    ..

--- app.js
+++ app.js
@@ -2,6 +2,54 @@ window.addEventListener('load', () => {
     var form = document.querySelector('#new-task-form');
     var input = document.querySelector('#new-task-input');
     var list_el = document.querySelector('#tasks');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-    console.log(form);
+    var task = input.value;
+    if(!task) {
+        alert("Please fill out the task");
+        return;
+    } 
+    var task_el = document.createElement("div");
+    task_el.classList.add("task");
+
+    var task_content_el = document.createElement("div");
+    task_content_el.classList.add("content");
+    
+
+    task_el.appendChild(task_content_el);
+
+    var task_input_el = document.createElement("input");
+    task_input_el.classList.add("text");
+    task_input_el.type = "text";
+    task_input_el.value = task;
+    task_input_el.setAttribute("readonly", "readonly");
+
+    task_content_el.appendChild(task_input_el);
+
+    var task_actions_el = document.createElement("div");
+    task_actions_el.classList.add("actions");
+
+    var task_edit_el = document.createElement("button");
+    task_edit_el.classList.add("edit");
+    task_edit_el.innerHTML = "Edit";
+
+    var task_delete_el = document.createElement("button");
+    task_delete_el.classList.add("delete");
+    task_delete_el.innerHTML = "Delete";
+
+    task_actions_el.appendChild(task_edit_el);
+    task_actions_el.appendChild(task_delete_el);
+
+    task_el.appendChild(task_actions_el);
+
+    list_el.appendChild(task_el);
+
+    input.value = "";
+
+    task_delete_el.addEventListener('click', () => {
+        task_input_el.removeAttribute("readonly");
+        task_input_el.focus();
+    })
+})
 })

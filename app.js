b42bda319b4948430928d4bc0c9a4fc2dd8e5361
mehmood <mehmoodalam60632@gmail.com>
2022-05-27 17:03:50 +0500

    ...

---
+++ app.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', () => {
+    var form = document.querySelector('#new-task-form');
+    var input = document.querySelector('#new-task-input');
+    var list_el = document.querySelector('#tasks');
+
+    console.log(form);
+})
